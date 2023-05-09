9417b71df0de14d8cde55b00d4a4b487411eb471
Sameh A. Elalfi <sameh.elalfi.mail@gmail.com>
2023-05-10 00:00:50 +0300

    chore: install and configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.js', '*.jsx'],
+      rules: {
+        '@nx/enforce-module-boundaries': [
+          'error',
+          {
+            // ...rule specific configuration
+            // @see https://nx.dev/packages/eslint-plugin/documents/enforce-module-boundaries
+          }
+        ]
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  }
+}
